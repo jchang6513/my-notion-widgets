34a226a8bfdc25cb2181dc37d0f95fe8d3e13269
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2021-07-13 20:43:28 +0800

    ✨ Add block of latest WPM (#1)
    * Add latest wpm page
    
    * Fix build

---
+++ src/helpers/envVariables.ts
@@ -0,0 +1,6 @@
+export const workspace = process.env.TOKEN as string;
+
+export const databaseSecrete = {
+  widgetList: process.env.WIDGET_LIST as string,
+  latestWpm: process.env.WIDGET_LASTEST_WPM as string,
+};
---
+++ src/models/Widget.ts
@@ -0,0 +1,22 @@
+import { Database } from 'models/Database';
+
+export type Widget = {
+  name: string;
+  pathname: string;
+};
+
+export const dbToWidgetList = (database: Database): Widget[] => database.data.reduce<Widget[]>((acc, data) => {
+  const name = data.Name?.title?.name;
+  const pathname = data.Pathname?.url;
+
+  if (name && pathname) {
+    return [
+      ...acc,
+      {
+        name,
+        pathname,
+      },
+    ];
+  }
+  return acc;
+}, []);
